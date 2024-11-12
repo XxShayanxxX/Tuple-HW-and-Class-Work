54800ab04f240adca74fc321bb0b18905465d51a
XxShayanxxX <balajishayan@gmail.com>
2024-11-12 18:44:34 +0800

    first commit

---
+++ tuple.py
@@ -0,0 +1,14 @@
+tuple1 = (1 , False , "Coding" , 9.8)
+print(tuple1)
+
+tuple2 = (1 ,2 ,3 ,4 ,5 ,6 ,7)
+print(tuple2)
+
+tuple3 = (9,)
+print(tuple2 + tuple3)
+
+tuple4 = ( 1, 5, 1, 32, 13, 1, 3, 1, 7, 1)
+print(tuple4.count(1))
+
+tuple5 = (3 , 4, 5 , 6 ,7 , 8, 9, 0, 1 , 5)
+print(tuple5[3:7])
